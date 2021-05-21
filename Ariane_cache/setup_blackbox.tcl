14466ae028badf7d47ca7cfda732a5d435480f93
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2021-05-21 16:20:31 +0200

    Fixed file format. Added blackbox script

--- Ariane_cache/setup_blackbox.tcl
+++ Ariane_cache/setup_blackbox.tcl
@@ -7,8 +7,10 @@ read_verilog -golden  -pragma_ignore {}  -version sv2012 {SyncSpRamBeNx64.sv }
 read_verilog -golden  -pragma_ignore {}  -version sv2012 {lfsr_8bit.sv }
 read_verilog -golden  -pragma_ignore {}  -version sv2012 {lzc.sv }
 
-elaborate -golden -verbose -top cva6_icache
+elaborate -golden
+
+set_compile_option -golden -black_box_instances {  {gen_sram[0]/data_sram} {gen_sram[0]/tag_sram} {gen_sram[1]/data_sram} {gen_sram[1]/tag_sram} {gen_sram[2]/data_sram} {gen_sram[2]/tag_sram} {gen_sram[3]/data_sram} {gen_sram[3]/tag_sram} } -black_box {  } -cut_signal  {} -top {} -dontcare_handling sim -macro_iterations 4 -undriven_value sim -time_step macro -feedback_loop_latch {} -clock {} -no_clock {} -signal_domains {};
+
+compile -golden
 
 set_mode mv
-
-read_itl  {cache_property.vli}
